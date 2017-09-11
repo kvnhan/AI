99a57b66926378f39aca6d6c7a7fda9d05b85fe9
jmetzger <jmetzger@192.168.1.117>
2017-09-11 15:42:37 -0400

    title

--- src/SetUp/Main.java
+++ src/SetUp/Main.java
@@ -16,14 +16,14 @@ public class Main {
 		Graph g = new Graph();
 		Initial i = new Initial();
 
-		System.out.println("\n===== Project 1 =====");
-		System.out.println("\nPlease Select the Search Algorithm below you wish to execute:");
+		System.out.println("\n========================================\n=============== Project 1 ==============\n============ CS4341 : 9.11.17 ==========\n=============== Kien Nhan ==============\n============ Jonathan Metzger ==========\n============= Joseph Wilder ============\n========================================");
+		System.out.println("\nPlease Select the Search Algorithm below\nyou wish to execute:");
 		System.out.println ( "\n0) Print All\n1) Depth 1st Search\n2) Breadth 1st Search\n3) Depth-Limited Search\n4) Iterative Deepening Search\n5) Uniform Cost Search (Branch-and-bound)\n6) Greedy Search\n7) A*\n8) Hill-Climbing Search\n9) Beam Search\n" );
 	    System.out.print ( "Selection: " );
 		
 	    i.parse(br, fr, args, g);
 	    i.selection(g);
 		
-	    System.out.println("\n===== EOF =====\n\n");
+	    System.out.println("\n================== EOF =================\n\n");
 	}
 }
